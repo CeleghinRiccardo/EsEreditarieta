df194e14d920f77bda52484971f67896e6369f45
Celeghin Riccardo Maria <celeghin.riccardo@osdbsesto>
2025-10-15 09:53:28 +0200

    Modificata la Classe Armadio

--- src/Armadio.java
+++ src/Armadio.java
@@ -1,3 +1,47 @@
+import java.util.Scanner;
+
 public class Armadio {
 
+
+    public Scanner sc = new Scanner(System.in);
+    public static final int dimensione=10;
+    private Smartphone smart[] = new Smartphone[dimensione];
+    private int puntatore;
+
+
+    public Armadio(int puntatore){
+        puntatore=0;
+    }
+
+    public void pushSmart (String marca, String modello, int annoProd, int nSim){
+        if (puntatore<dimensione){
+
+            System.out.println("inserisci marca: ");
+            marca=sc.next();
+            System.out.println("inserisci modello: ");
+            modello=sc.next();
+            System.out.println("inserisci Anno di produzione: ");
+            annoProd=sc.nextInt();
+            System.out.println("inserisci Numero di SIM: ");
+            nSim=sc.nextInt();
+
+            smart[puntatore]=new Smartphone(smart[puntatore].getMarca(), smart[puntatore].getModello(), smart[puntatore].getAnnoProd(), smart[puntatore].getnSim());
+            puntatore++;
+
+
+        }else{
+            System.out.println("Armadio Pieno");
+        }
+    }
+
+    public int cercaSmart(String marca, String modello){
+        for (int i = 0; i < smart.length; i++){
+            if(smart[i] != null) {
+                if (smart[i].getMarca().equals(marca) && smart[i].getModello().equals(modello)) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
 }
